7ae33e56acfe0609528aab914ce04f9fbed42801
Yasha Bubnov <girokompass@gmail.com>
2022-02-08 20:29:36 +0300

    Rename `CoordinateSpace` -> `ContentDisposition`
    Also this patch formats the source code using `swift-format`.

--- Sources/PlotUI/Tick.swift
+++ Sources/PlotUI/Tick.swift
@@ -1,21 +1,20 @@
 import Foundation
 import SwiftUI
 
-
 public protocol TickLabelStyle {
-    associatedtype Body : View
-    
+    associatedtype Body: View
+
     func position(axis: CGRect, tick: Text) -> Body
 }
 
 public struct AnyTickLabelStyle: TickLabelStyle {
-    
+
     public typealias Body = AnyView
-    
+
     private let parentPosition: (CGRect, Text) -> AnyView
 
     public init<S: TickLabelStyle>(_ style: S) {
-        self.parentPosition = {(axis: CGRect, tick: Text) in
+        self.parentPosition = { (axis: CGRect, tick: Text) in
             AnyView(style.position(axis: axis, tick: tick))
         }
     }
@@ -27,17 +26,17 @@ public struct AnyTickLabelStyle: TickLabelStyle {
 
 public struct TickLabelStyleConfiguration {
     /// A type-erased label of a tick.
-    public struct Label : View {
+    public struct Label: View {
         /// The type of view representing the body of this view.
         public var body: Never
     }
-    
+
     public let axis: CGRect
-    
+
     public let label: TickLabelStyleConfiguration.Label
 
     public let fontSize: CGFloat = 10
-    
+
     public let fontWeight: Font.Weight = .light
 }
 
@@ -51,21 +50,19 @@ extension TickLabelStyle where Self == BottomTickLabelStyle {
     public static var bottom: BottomTickLabelStyle { BottomTickLabelStyle() }
 }
 
-
 extension TickLabelStyle where Self == BottomTrailingTickLabelStyle {
-    public static var bottomTrailing: BottomTrailingTickLabelStyle { BottomTrailingTickLabelStyle() }
+    public static var bottomTrailing: BottomTrailingTickLabelStyle {
+        BottomTrailingTickLabelStyle()
+    }
 }
-
 
 extension TickLabelStyle where Self == TrailingTickLabelStyle {
     public static var trailing: TrailingTickLabelStyle { TrailingTickLabelStyle() }
 }
 
-
 extension TickLabelStyle where Self == EmptyTickLabelStyle {
     public static var empty: EmptyTickLabelStyle { EmptyTickLabelStyle() }
 }
-
 
 public struct EmptyTickLabelStyle: TickLabelStyle {
     public func position(axis: CGRect, tick: Text) -> some View {
@@ -73,77 +70,76 @@ public struct EmptyTickLabelStyle: TickLabelStyle {
     }
 }
 
-
 public struct BottomTickLabelStyle: TickLabelStyle {
     public var fontSize: CGFloat = 10
     public var fontWeight: Font.Weight = .light
-    
+
     public func position(axis: CGRect, tick: Text) -> some View {
         let pos = CGSize(width: axis.maxX, height: axis.maxY + fontSize)
-        
-        return tick
+
+        return
+            tick
             .font(font())
             .position(x: pos.width, y: pos.height)
     }
-    
+
     public func font() -> Font {
         return .system(size: fontSize, weight: fontWeight)
     }
 }
-
 
 public struct BottomTrailingTickLabelStyle: TickLabelStyle {
     public var fontSize: CGFloat = 10
     public var fontWeight: Font.Weight = .light
-    
+
     public func position(axis: CGRect, tick: Text) -> some View {
         let pos = CGSize(width: axis.maxX, height: axis.maxY)
 
-        return tick
+        return
+            tick
             .font(font())
-            .offset(x: pos.width + fontSize/4, y: pos.height - fontSize)
+            .offset(x: pos.width + fontSize / 4, y: pos.height - fontSize)
     }
-    
+
     public func font() -> Font {
         return .system(size: fontSize, weight: fontWeight)
     }
 }
-
 
 public struct TrailingTickLabelStyle: TickLabelStyle {
     public var fontSize: CGFloat = 10
     public var fontWeight: Font.Weight = .light
-    
-    public func position(axis: CGRect, tick: Text) -> some View {
-        let pos = CGSize(width: axis.maxX + fontSize/3, height: axis.maxY - (fontSize * 2 / 3))
 
-        return tick
+    public func position(axis: CGRect, tick: Text) -> some View {
+        let pos = CGSize(width: axis.maxX + fontSize / 3, height: axis.maxY - (fontSize * 2 / 3))
+
+        return
+            tick
             .font(font())
             .offset(x: pos.width, y: pos.height)
     }
-    
+
     public func font() -> Font {
         return .system(size: fontSize, weight: fontWeight)
     }
 }
 
-
 public struct TickLabel: View {
     public var label: LocalizedStringKey
     public var axis: Path
-    
+
     public init(_ label: LocalizedStringKey, axis: Path) {
         self.label = label
         self.axis = axis
     }
-    
+
     public var body: some View {
         Text(label)
     }
 }
 
 extension TickLabel {
-    
+
     public func tickLabelStyle<S: TickLabelStyle>(_ style: S) -> some View {
         GeometryReader { rect in
             style.position(axis: axis.boundingRect, tick: Text(label))
@@ -151,19 +147,18 @@ extension TickLabel {
     }
 }
 
-
 extension Shape {
     private func boundingRect(_ rect: GeometryProxy) -> CGRect {
         return CGRect(origin: CGPoint(), size: rect.size)
     }
-    
+
     public func tickLabel<S: TickLabelStyle>(_ label: LocalizedStringKey, style: S) -> some View {
         GeometryReader { rect in
             TickLabel(label, axis: path(in: boundingRect(rect))).tickLabelStyle(style)
             self
         }
     }
-    
+
     public func tickLabel(_ label: LocalizedStringKey) -> some View {
         GeometryReader { rect in
             TickLabel(label, axis: path(in: boundingRect(rect)))
--- Sources/PlotUI/TickShape.swift
+++ Sources/PlotUI/TickShape.swift
@@ -1,32 +1,30 @@
 import Foundation
 import SwiftUI
 
-
 public protocol TickShape: Shape {
-    init(_ space: CoordinateSpace, at: CGFloat)
+    init(_ disposition: ContentDisposition, at: CGFloat)
 }
-
 
 public struct VerticalTick: TickShape {
     private var at: CGFloat
-    
+
     // TODO: move x-tick and y-tick sizes into observed object.
     private var xtick: CGFloat = 40
     private var ytick: CGFloat = 20
-    
-    @ObservedObject var space: CoordinateSpace
-    
-    public init(_ space: CoordinateSpace, at: CGFloat) {
+
+    @ObservedObject var disposition: ContentDisposition
+
+    public init(_ disposition: ContentDisposition, at: CGFloat) {
         self.at = at
-        self.space = space
+        self.disposition = disposition
     }
-    
+
     public func path(in rect: CGRect) -> Path {
-        let xScale = (rect.size.width - xtick) / space.width
+        let xScale = (rect.size.width - xtick) / disposition.width
         let yOffset = rect.size.height - ytick
-        
-        let x = (at - space.xlim.left) * xScale
-        
+
+        let x = (at - disposition.xlim.left) * xScale
+
         return Path { path in
             path.move(to: CGPoint(x: x, y: yOffset))
             path.addLine(to: CGPoint(x: x, y: 0))
@@ -34,30 +32,29 @@ public struct VerticalTick: TickShape {
     }
 }
 
-
 public struct HorizontalTick: TickShape {
     private var at: CGFloat
 
     private var xtick: CGFloat = 40
     private var ytick: CGFloat = 20
 
-    @ObservedObject var space: CoordinateSpace
-    
-    public init(_ space: CoordinateSpace, at: CGFloat) {
+    @ObservedObject var disposition: ContentDisposition
+
+    public init(_ disposition: ContentDisposition, at: CGFloat) {
         self.at = at
-        self.space = space
+        self.disposition = disposition
     }
-    
+
     public func path(in rect: CGRect) -> Path {
-        let limit = space.height
-        
+        let limit = disposition.height
+
         // All y-axis ticks are located on the horizontal axis, hence subtract the
         // size of y-tick from the width of this axis.
         let xOffset = rect.size.width - ytick
         let yScale = (rect.size.height - xtick) / limit
 
-        let y = (rect.size.height - xtick) - (at - space.ylim.bottom) * yScale
-        
+        let y = (rect.size.height - xtick) - (at - disposition.ylim.bottom) * yScale
+
         return Path { path in
             path.move(to: CGPoint(x: 0, y: y))
             path.addLine(to: CGPoint(x: xOffset, y: y))
